a5c3d12f093c976d16948271041e2efbc250ad3b
InjectGG-svg <inject1228@gmail.com>
2024-12-29 13:03:14 +0800

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Adding scammers...", "Don't trust anyone..."];
+const statusMessages = ["ATM", "Giveaway"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
